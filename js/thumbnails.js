0930d2a7974ec2622d839e02f84a67489cd53f1a
Бородулин Дмитрий <mequeL0721@gmail.com>
2025-12-08 19:19:38 +0500

    Создание модуля для отрисовки

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,29 +1,27 @@
+import { openBigPicture } from './big-picture.js';
+
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
 
-const createThumbnail = (data) => {
-  const thumbnail = pictureTemplate.cloneNode(true);
-
-  const image = thumbnail.querySelector('.picture__img');
-  image.src = data.url;
-  image.alt = data.description;
-
-  thumbnail.querySelector('.picture__likes').textContent = data.likes;
-  thumbnail.querySelector('.picture__comments').textContent = data.comments.length;
-
-  return thumbnail;
-};
-
-const renderThumbnails = (photos) => {
+export const renderThumbnails = (data) => {
   const fragment = document.createDocumentFragment();
 
-  photos.forEach((photo) => {
-    const thumbnail = createThumbnail(photo);
+  data.forEach((picture) => {
+    const thumbnail = pictureTemplate.cloneNode(true);
+
+    const image = thumbnail.querySelector('.picture__img');
+    image.src = picture.url;
+    image.alt = picture.description;
+
+    thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+    thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
+
+    thumbnail.addEventListener('click', () => {
+      openBigPicture(picture);
+    });
+
     fragment.appendChild(thumbnail);
   });
 
   picturesContainer.appendChild(fragment);
 };
-
-export { renderThumbnails };
-
