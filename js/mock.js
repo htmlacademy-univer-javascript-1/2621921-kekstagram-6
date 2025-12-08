3417eca05721122062e04879f33b8fc55f8eeffb
Бородулин Дмитрий <mequeL0721@gmail.com>
2025-12-08 20:09:36 +0500

    Создание временных данных для разработки

---
+++ js/mock.js
@@ -0,0 +1,23 @@
+export const pictures = [
+  {
+    id: 1,
+    url: 'images/1.jpg',
+    description: 'Описание первого изображения',
+    likes: 25,
+    comments: 5
+  },
+  {
+    id: 2,
+    url: 'images/2.jpg',
+    description: 'Описание второго изображения',
+    likes: 71,
+    comments: 11
+  },
+  {
+    id: 3,
+    url: 'images/3.jpg',
+    description: 'Описание третьего изображения',
+    likes: 93,
+    comments: 7
+  },
+];
