615f20b570e03abd69eb7ad10a5bf4cb0c3a53d2
Бородулин Дмитрий <mequeL0721@gmail.com>
2025-12-08 22:50:45 +0500

    обновлена структура комментариев

--- js/mock.js
+++ js/mock.js
@@ -1,24 +1,53 @@
-// Временные данные для разработки
-export const pictures = [
+const picturesData = [
   {
     id: 1,
     url: 'photos/1.jpg',
-    description: 'Описание фотографии 1',
+    description: 'Красивый закат на море',
     likes: 25,
-    comments: 5
+    comments: [
+      {
+        avatar: 'img/avatar-1.svg',
+        name: 'Андрей',
+        message: 'Отличное фото!'
+      },
+      {
+        avatar: 'img/avatar-2.svg',
+        name: 'Мария',
+        message: 'Какая красота!'
+      }
+    ]
   },
   {
     id: 2,
     url: 'photos/2.jpg',
-    description: 'Описание фотографии 2',
+    description: 'Горный пейзаж',
     likes: 71,
-    comments: 11
+    comments: [
+      {
+        avatar: 'img/avatar-3.svg',
+        name: 'Игорь',
+        message: 'Супер!'
+      }
+    ]
   },
   {
     id: 3,
     url: 'photos/3.jpg',
-    description: 'Описание фотографии 3',
+    description: 'Осенний лес',
     likes: 93,
-    comments: 7
-  },
+    comments: [
+      {
+        avatar: 'img/avatar-4.svg',
+        name: 'Оля',
+        message: 'Шикарно!'
+      },
+      {
+        avatar: 'img/avatar-5.svg',
+        name: 'Дима',
+        message: 'Хочу туда :)'
+      }
+    ]
+  }
 ];
+
+export { picturesData };
