611866298a331504dd2017a6eff3edf6c1b512ef
Бородулин Дмитрий <mequeL0721@gmail.com>
2025-12-08 20:26:40 +0500

    Временные данные

--- js/mock.js
+++ js/mock.js
@@ -1,22 +1,23 @@
+
 export const pictures = [
   {
     id: 1,
-    url: 'images/1.jpg',
-    description: 'Описание первого изображения',
+    url: 'photos/1.jpg',
+    description: 'Описание фотографии 1',
     likes: 25,
     comments: 5
   },
   {
     id: 2,
-    url: 'images/2.jpg',
-    description: 'Описание второго изображения',
+    url: 'photos/2.jpg',
+    description: 'Описание фотографии 2',
     likes: 71,
     comments: 11
   },
   {
     id: 3,
-    url: 'images/3.jpg',
-    description: 'Описание третьего изображения',
+    url: 'photos/3.jpg',
+    description: 'Описание фотографии 3',
     likes: 93,
     comments: 7
   },
